1b5e1ccb60832621a2c7d2a584ccc43f48b83db2
Jarrett Cruger <jcruger@qrhook.com>
2012-09-30 04:01:29 -0400

    Added database insert attempt to the socket event

--- binarySocket.js
+++ binarySocket.js
@@ -9,17 +9,37 @@ var BinaryServer = require('binaryjs').BinaryServer,
 
 binarySocket.start = function (server, callback) {
 
+	var pool = new Pool(require('http'), ["127.0.0.1:8098"]);
+
 	bs = BinaryServer({server: server});
 
 	bs.on('connection', function (client) {
 
 		client.on('stream', function (stream, meta) {
 
-			var file = filed(__dirname + '/public/uploads/' + meta.name);
+			// var file = filed(__dirname + '/public/uploads/' + meta.name);
 
-			stream.pipe(file);
+			// stream.pipe(file);
 
+			pool.request({
+				path: '/riak/pictures/' + meta.name,
+				method: 'PUT',
+				data: stream,
+				headers: {'Content-type': 'image/png'},
+				stream: true
+			}, function(err, res, body) {
 
+				if(!err && res) {
+
+					callback(null, res);
+
+				} else {
+
+					callback(err, null);
+
+				}
+
+			});
 
 			stream.on('data', function (data) {
 
@@ -27,11 +47,6 @@ binarySocket.start = function (server, callback) {
 
 			});
 
-			stream.on('end', function () {
-
-
-
-			});
 		});
 
 
